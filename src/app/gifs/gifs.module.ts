8a1cd92549d1266d4aa1db2d81b6561b6f960583
mullet <mullet@gmail.com>
2024-03-20 15:55:32 -0300

    gifsapp initial commit

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomePageComponent } from './pages/home/home-page.components';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    SearchBarComponent,
+    CardListComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomePageComponent
+  ]
+})
+export class GifsModule { }
---
+++ src/app/gifs/pages/home/home-page.components.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-home-page',
+  templateUrl: './home-page.components.html'
+})
+
+export class HomePageComponent {
+  constructor() { }
+
+}
